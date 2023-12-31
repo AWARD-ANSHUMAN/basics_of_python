b6a1047ce902aca4927aee9baec1ccf61f401c35
AWARD-ANSHUMAN <95232390+AWARD-ANSHUMAN@users.noreply.github.com>
2023-12-31 14:14:36 +0530

    pyspark basics

---
+++ pyspark.py
@@ -0,0 +1,4 @@
+
+# https://towardsdatascience.com/pyspark-and-sparksql-basics-6cb4bf967e53
+
+# https://medium.com/geekculture/pandas-vs-pyspark-fe110c266e5c#:~:text=Key%20Differences%20between%20PySpark%20and%20Pandas&text=PySpark%20allows%20for%20parallel%20processing,data%20from%20local%20file%20systems.
